584b3ecafb2b77fcbf658d2c4bcfd4e259e17355
大西友香 <oonishiyuka@oonishiyukanoMacBook-ea.local>
2020-02-26 18:21:14 +0900

    testinfo

--- page-testinfo.php
+++ page-testinfo.php
@@ -3,183 +3,146 @@ $home = esc_url(home_url());
 $wp_url = get_template_directory_uri();
 get_header(); ?>
 
-<section id="point" class="mt-3">
+<section id="info" class="mt-3">
+  <div class="pnk_bg pb-2">
+    <div class="wrap">
+      <h2 class="txt-c pt-2 mb-2">保育士試験の情報</h2>
+      <div class="table-01">
+      <?php
+      $arg = array(
+        'posts_per_page' => 1,
+        'orderby' => 'date',
+        'order' => 'DESC',
+        'post_type' => 'info',
+      );
+      $posts = get_posts($arg);
+      if ($posts): ?>
+      <?php foreach ($posts as $post): setup_postdata($post);
+      ?>
+        <dl class="ac">
+          <dt><?php the_field( 'year' ); ?>前期(<?php the_field( 'day_01_01' ); ?>,<?php the_field( 'day_01_02' ); ?>,<?php the_field( 'day_01_03' ); ?>)</dt>
+          <dd>
+            <h3>前期(1回目)試験 筆記試験</h3>
+            <h4><?php the_field( 'day_01_01' ); ?></h4>
+            <div class="table"><?php the_field( 'timetable_01_01' ); ?></div>
+            <h4><?php the_field( 'day_01_02' ); ?></h4>
+            <div class="table"><?php the_field( 'timetable_01_02' ); ?></div>
+            <h3>前期(1回目)試験 筆記試験</h3>
+            <p><?php the_field( 'day_01_03' ); ?></p>
+          </dd>
+        </dl>
+        <dl class="ac">
+          <dt><?php the_field( 'year' ); ?>前期(<?php the_field( 'day_02_01' ); ?>,<?php the_field( 'day_02_02' ); ?>,<?php the_field( 'day_02_03' ); ?>)</dt>
+          <dd>
+            <h3>前期(2回目)試験 筆記試験</h3>
+            <h4><?php the_field( 'day_02_01' ); ?></h4>
+            <div class="table"><?php the_field( 'timetable_02_01' ); ?></div>
+            <h4><?php the_field( 'day_02_02' ); ?></h4>
+            <div class="table"><?php the_field( 'timetable_02_02' ); ?></div>
+            <h3>前期(3回目)試験 筆記試験</h3>
+            <p><?php the_field( 'day_02_03' ); ?></p>
+          </dd>
+        </dl>
+      <?php endforeach; ?>
+      <?php endif; wp_reset_postdata(); ?>
+      </div>
+    </div>
+  </div>
   <div class="wrap">
-    <ul>
-      <li class="mb-2">
-        <div class="ttl sp-only">
-          <p class="num">01</p>
-          <h3>塾のプロが<br><span class="maker">あなたの勉強</span>をサポート。</h3>
-        </div>
-        <div class="img-wrap">
-        </div>
-        <div class="txt-wrap">
-          <div class="ttl pc-only">
-            <p class="num">01</p>
-            <h3>塾のプロが<br><span class="maker">あなたの勉強</span>をサポート。</h3>
-          </div>
-          <p>これから保育士を運営する株式会社京進が持つ学習塾のノウハウを活かして、資格取得に最適な試験対策を提供します。</p>
-        </div>
-      </li>
-      <li class="mb-2">
-        <div class="ttl sp-only">
-          <p class="num">02</p>
-          <h3>スマホやタブレットで<br><span class="maker">手軽に勉強</span>できる。</h3>
-        </div>
-        <div class="img-wrap">
-        </div>
-        <div class="txt-wrap">
-          <div class="ttl pc-only">
-            <p class="num">02</p>
-            <h3>スマホやタブレットで<br><span class="maker">手軽に勉強</span>できる。</h3>
-          </div>
-          <p>これから保育士はインターネットで学べるeラーニングです。パソコンはもちろん、スマホやタブレットがあれば、いつでもどこでも資格の勉強ができます。</p>
-        </div>
-      </li>
-      <li class="">
-        <div class="ttl sp-only">
-          <p class="num">03</p>
-          <h3><span class="maker">月々1,980円〜</span>で<br>負担なく資格を取得。</h3>
-        </div>
-        <div class="img-wrap">
-        </div>
-        <div class="txt-wrap">
-          <div class="ttl pc-only">
-            <p class="num">03</p>
-            <h3><span class="maker">月々1,980円〜</span>で<br>負担なく資格を取得。</h3>
-          </div>
-          <p>これから保育士を運営する株式会社京進が持つ学習塾のノウハウを活かして、資格取得に最適な試験対策を提供します。</p>
-        </div>
-      </li>
-    </ul>
+    <div class="table-02 pt-2">
+      <dl class="ac">
+        <dt>受験料</dt>
+        <dd>12,700円（税込）＋250円（郵送料）＝12,950円</dd>
+      </dl>
+      <dl class="ac">
+        <dt>受験資格</dt>
+        <dd>１．大学卒業者
+          <br>２．大学中退又は在学中で62単位修得済み又は習得見込みがあると学校長が認めた方。
+          <br>３．短大卒業
+          <br>４．短大在学中で卒業見込みがある
+          <br>５．2年以上通う専門学校卒業
+          <br>６．2年以上通う専門学校に在学中で卒業見込みがある
+          <br>７．平成3年3月31日以前に高校卒業
+          <br>８．平成8年3月31日以前に保育科高校卒業
+          <br>９．中学・高校卒業後（同等の資格を有する方含む）児童福祉施設において2年以上かつ2880時間以上従事し実務経験を積んだ方
+          <br>１０．中学卒業後、児童福祉施設において5年以上かつ7200時間以上従事した方
+          <br>１１．平成３年（1991年）3月31日前までに高校を卒業された方</dd>
+      </dl>
+    </div>
   </div>
 </section>
 
-<div class="ttl-img">
+<div class="ttl-img schedule">
   <div class="ttl-img-bg">
-    <img src="<?php echo $wp_url ?>/lib/images/about/icon_feature.png" alt="feature">
-    <p>feature</p>
+    <img src="<?php echo $wp_url ?>/lib/images/testinfo/icon_schedule.png" alt="schedule">
+    <p>schedule</p>
   </div>
 </div>
 
-<section id="feature" class="bg-gray pt-4 mb-3">
+<section id="schedule" class="bg-gray pt-4 mb-3">
   <div class="wrap">
-    <p class="txt-c">これまでの試験勉強とは違う！</p>
-    <h2 class="ttl2 mb-2"><span class="maker">ムリなく進められる理由</span></h2>
+    <h2 class="ttl2">どうやって<br><span class="maker">これから保育士</span>で合格したの？</h2>
+    <p class="txt-c mb-2">Aさんの場合</p>
     <ul class="">
       <li>
-        <div class="img-wrap">
-          <img src="<?php echo $wp_url ?>/lib/images/about/feature_01.png" alt="イラスト">
+        <div class="ttl">
+          <p>試験まで<span>4週間</span></p>
+          <h3>まずは動画で基礎を勉強</h3>
         </div>
-        <h3>山のような参考書は<br>届きません！</h3>
-        <p>インターネットで学習できるeラーニングだから、テキストや問題集よりもスマートに、机に向かわなくても勉強できます。</p>
+        <div class="img-wrap">
+          <img src="<?php echo $wp_url ?>/lib/images/testinfo/01.png" alt="合格までの様子">
+        </div>
+        <p>知識をインプットしたあとはひたすら一問一答!間違った問題は正解するまで解き続けました。スマホでできるので、ゲーム感覚で楽しく勉強できました。</p>
       </li>
       <li>
-        <div class="img-wrap">
-          <img src="<?php echo $wp_url ?>/lib/images/about/feature_02.png" alt="イラスト">
+        <div class="ttl">
+          <p>試験まで<span>3週間</span></p>
+          <h3>解説動画で知識を深める</h3>
         </div>
-        <h3>毎日10分~だから<br>続けられる</h3>
-        <p>講義動画や一問一答形式の問題などスキマ時間でも勉強しやすい教材だから、一日10分からでも勉強できます。</p>
+        <div class="img-wrap">
+          <img src="<?php echo $wp_url ?>/lib/images/testinfo/02.png" alt="合格までの様子">
+        </div>
+        <p>過去問を解く前に、重要ポイントを動画で学びました。実際の試験問題や自分がつまづきそうなところを解消していきました。</p>
       </li>
       <li>
-        <div class="img-wrap">
-          <img src="<?php echo $wp_url ?>/lib/images/about/feature_03.png" alt="イラスト">
+        <div class="ttl">
+          <p>試験まで<span>2週間</span></p>
+          <h3>1問1答を繰り返す</h3>
         </div>
-        <h3>脳科学に基づいた勉強法で<br>効率的な勉強をサポート</h3>
-        <p>重要テーマをインプットして、繰り返し演習でアウトプットすることで、効率よく勉強できます。</p>
+        <div class="img-wrap">
+          <img src="<?php echo $wp_url ?>/lib/images/testinfo/03.png" alt="合格までの様子">
+        </div>
+        <p>知識をインプットしたあとはひたすら一問一答!間違った問題は正解するまで解き続けました。スマホでできるので、ゲーム感覚で楽しく勉強できました。</p>
       </li>
-    </ul>
-  </div>
-</section>
-
-<section id="teacher" class="mb-3">
-  <div class="wrap">
-    <div class="ttl-img">
-      <img src="<?php echo $wp_url ?>/lib/images/about/icon_teacher.png" alt="feature">
-      <p>teacher</p>
-    </div>
-    <h2 class="ttl2 mb-2"><span class="maker">講師紹介</span></h2>
-    <ul>
-      <li class="flex">
+      <li>
+        <div class="ttl">
+          <p>試験まで<span>1週間</span></p>
+          <h3>過去問にチャレンジ</h3>
+        </div>
         <div class="img-wrap">
-          <img src="<?php echo $wp_url ?>/lib/images/about/teacher/01.jpg" alt="講師イメージ">
+          <img src="<?php echo $wp_url ?>/lib/images/testinfo/04.png" alt="合格までの様子">
         </div>
-        <div class="txt-wrap">
-          <h3>橋本 圭介<span>講師</span></h3>
-          <p>三幸学園大宮こども専門学校専任講師／豊岡短期大学非常勤講師／三幸学園東京リゾート＆スポーツ専門学校非常勤講師</p>
-        </div>
+        <p>総仕上げとして過去問を解きました。間違った問題はすぐに解答と解説が確認できるので、わからない部分が放ったらかしにならずに、理解まで辿り着くことができました。</p>
       </li>
-      <li class="flex">
+      <li>
+        <div class="ttl">
+          <p>試験<span>本番</span></p>
+          <h3>試験の待ち時間で一問一答</h3>
+        </div>
         <div class="img-wrap">
-          <img src="<?php echo $wp_url ?>/lib/images/about/teacher/02.jpg" alt="講師イメージ">
+          <img src="<?php echo $wp_url ?>/lib/images/testinfo/05.png" alt="合格までの様子">
         </div>
-        <div class="txt-wrap">
-          <h3>喜多野 直子<span>講師</span></h3>
-          <p>管理栄養士。医療・保育系の短大や専門学校で食に関わる授業を担当／特定保健所の指導員／保育士試験対策校講師</p>
-        </div>
+        <p>試験当日は移動中や待ち時間にもスマホで最終確認!このとき解いた問題が実際に試験にも出たので嬉しかったです。</p>
       </li>
-      <li class="flex">
+      <li>
+        <div class="ttl">
+          <p>緊張の<span>発表日</span></p>
+          <h3>合格！</h3>
+        </div>
         <div class="img-wrap">
-          <img src="<?php echo $wp_url ?>/lib/images/about/teacher/03.jpg" alt="講師イメージ">
+          <img src="<?php echo $wp_url ?>/lib/images/testinfo/06.png" alt="合格までの様子">
         </div>
-        <div class="txt-wrap">
-          <h3>藤井 秀一<span>講師</span></h3>
-          <p>NPO法人「親心支援協会」副理事長／プロフェッショナル・キャリア・カウンセラー／教育支援コーディネーター</p>
-        </div>
-      </li>
-      <li class="flex">
-        <div class="img-wrap">
-          <img src="<?php echo $wp_url ?>/lib/images/about/teacher/04.jpg" alt="講師イメージ">
-        </div>
-        <div class="txt-wrap">
-          <h3>中山 麻子<span>講師</span></h3>
-          <p>大学・および専門学校にて心理学講師。専門学校においてスクールカウンセラー（学生相談員）</p>
-        </div>
-      </li>
-      <li class="flex">
-        <div class="img-wrap">
-          <img src="<?php echo $wp_url ?>/lib/images/about/teacher/05.jpg" alt="講師イメージ">
-        </div>
-        <div class="txt-wrap">
-          <h3>喜多崎 薫<span>講師</span></h3>
-          <p>ヒューマンアカデミー「デザイン・こどもの造形教育」非常勤講師／あさか保育人材養成学校 保育士試験講座担当</p>
-        </div>
-      </li>
-      <li class="flex">
-        <div class="img-wrap">
-          <img src="<?php echo $wp_url ?>/lib/images/about/teacher/06.jpg" alt="講師イメージ">
-        </div>
-        <div class="txt-wrap">
-          <h3>今中 紀子<span>講師</span></h3>
-          <p>星槎教育研究所東京事務局スタッフ／幼児教室「あのね」代表／チャイルドカウンセラー／保育士</p>
-        </div>
-      </li>
-      <li class="flex">
-        <div class="img-wrap">
-          <img src="<?php echo $wp_url ?>/lib/images/about/teacher/07.jpg" alt="講師イメージ">
-        </div>
-        <div class="txt-wrap">
-          <h3>小渕 隆志<span>講師</span></h3>
-          <p>新国際福祉カレッジ専任教員／在宅サービスの介護職、ケアマネージャーを経て介護仕事における人材の教育活動</p>
-        </div>
-      </li>
-      <li class="flex">
-        <div class="img-wrap">
-          <img src="<?php echo $wp_url ?>/lib/images/about/teacher/08.jpg" alt="講師イメージ">
-        </div>
-        <div class="txt-wrap">
-          <h3>大江 美沙<span>講師</span></h3>
-          <p>あさか保育人材養成学校 保育実習理論・音楽 実技のピアノ指導担当／幼児教室 音符ビッツ教室 ピッコリーナ開校</p>
-        </div>
-      </li>
-      <li class="flex">
-        <div class="img-wrap">
-          <img src="<?php echo $wp_url ?>/lib/images/about/teacher/09.jpg" alt="講師イメージ">
-        </div>
-        <div class="txt-wrap">
-          <h3>有路 智恵<span>講師</span></h3>
-          <p>経済連携協定（ＥＰＡ）外国人看護師候補者家試験対策／保育士国家試験対策「子どもの健」非常勤講師</p>
-        </div>
+        <p>無事に合格!スマホだけで勉強を進められたので、仕事をしながらでも合格できました。eラーニングは初めてだったけど、かなり便利なので他の人にもおすすめしたいです。</p>
       </li>
     </ul>
   </div>
