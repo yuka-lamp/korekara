ff3a3c9f26923bace61bc5756a74ab5e3111ba15
大西友香 <oonishiyuka@oonishiyukanoMacBook-ea.local>
2020-02-27 16:53:51 +0900

    微調整

--- page-contact.php
+++ page-contact.php
@@ -10,13 +10,13 @@ get_header(); ?>
   </div>
 </section>
 
-<section id="contact-faq" class="sec">
+<section id="contact-faq" class="mb-3">
 <div class="wrap">
 <p class="txt-c">お客様からのよくあるご質問とその回答を掲載しています。<a href="<?php echo $home ?>/faq/" class="main-color">よくあるご質問</a><br>上記をご覧になっても疑問が解決しない場合はお問い合わせフォームか電話番号へお問い合わせ下さい。</p>
 </div>
 </section>
 
-<section id="contact-form">
+<section id="contact-form" class="pt-3 pb-3 mb-3">
 <div class="wrap">
 <?php echo do_shortcode('[contact-form-7 id="46" title="お問合せフォーム"]'); ?>
 </div>
